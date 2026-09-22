78ad3128e8a94dd40f9b44f3b0031d4c310a48d3
zaxlofful <33877007+zaxlofful@users.noreply.github.com>
2026-09-22 06:22:25 -0700

    syntax fix

--- valheim/valheim_bepinex_hexium/install_script.sh
+++ valheim/valheim_bepinex_hexium/install_script.sh
@@ -169,12 +169,12 @@ if [ ! -z "$V_MODPACK_URL" ]; then
         # Check if the extracted directory contains BepInEx folder or individual plugin folders
         if [ -d "$DEPENDENCY_TEMP_DIR/BepInEx" ]; then
             echo "Copying BepInEx directory as is"
-            cp -Rf "$DEPENDENCY_TEMP_DIR/BepInEx/" /mnt/server/BepInEx
+            cp -Rf "$DEPENDENCY_TEMP_DIR/BepInEx/." /mnt/server/BepInEx
         else
             for MOD_DIRECTORY in plugins patchers; do
                 if [ -d "$DEPENDENCY_TEMP_DIR/$MOD_DIRECTORY" ]; then
                     echo "Copying $MOD_DIRECTORY directory into BepInEx directory"
-                    cp -Rf "$DEPENDENCY_TEMP_DIR/$MOD_DIRECTORY/" "/mnt/server/BepInEx/$MOD_DIRECTORY"
+                    cp -Rf "$DEPENDENCY_TEMP_DIR/$MOD_DIRECTORY/." "/mnt/server/BepInEx/$MOD_DIRECTORY"
                 fi
             done
         fi
