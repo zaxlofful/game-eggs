3e7203be1da021949590c56c32257388281ec932
zaxlofful <33877007+zaxlofful@users.noreply.github.com>
2026-09-22 23:06:04 -0700

    Switch to apt-get for installing

--- valheim/valheim_bepinex_hexium/install_script.sh
+++ valheim/valheim_bepinex_hexium/install_script.sh
@@ -16,8 +16,8 @@ echo -e "${YELLOW}-----Install Valheim Dedicated Server via SteamCMD-----${NC}"
 echo -e "${BLUE}-------------------------------------------------------${NC}"
 
 echo -e "${YELLOW}Updating package lists and installing required system dependencies...${NC}"
-apt -y update
-apt -y --no-install-recommends --no-install-suggests install curl jq p7zip-full ca-certificates
+apt-get -qq -y update
+apt-get -qq-y --no-install-recommends --no-install-suggests install curl jq p7zip-full ca-certificates
 
 # Just in case someone removed the defaults.
 if [ "${STEAM_USER}" == "" ]; then
